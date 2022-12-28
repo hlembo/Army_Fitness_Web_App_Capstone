4a774d4f3b3c9ff3f147f4382c0d1e3bd16e7a12
Henry Lembo <lembohenry@gmail.com>
2022-12-28 05:57:48 -0500

    Update to overall Capstone Project
    This update to the capstone adds a few webpages such as cadre.html, mission.html, ect. They are added to the homepage as links through a cool CSS hexagon pattern that I fixed up and made fit what i need. From there I decided to implement an ACFT calculator however I ran into a bug with updating Gender selection and AGE-Group Selection bug in which female wasnt being changed until now i believe it is fixed!

--- website/views.py
+++ website/views.py
@@ -4,11 +4,38 @@ from flask_login import  login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/',  methods = ["GET","POST"])
-@login_required
+#@login_required
 def home():
     return render_template("home.html", user = current_user)
 
 @views.route('/cadre',  methods = ["GET","POST"])
-@login_required
 def cadre():
     return render_template("cadre.html", user = current_user)
+
+@views.route('/mission',  methods = ["GET","POST"])
+def mission():
+    return render_template("mission.html", user = current_user)
+
+@views.route('/scholar',  methods = ["GET","POST"])
+def scholar():
+    return render_template("scholar.html", user = current_user)
+
+@views.route('/ms',  methods = ["GET","POST"])
+def ms():
+    return render_template("ms.html", user = current_user)
+
+@views.route('/bh',  methods = ["GET","POST"])
+def bh():
+    return render_template("bh.html", user = current_user)
+
+@views.route('/upcoming',  methods = ["GET","POST"])
+def upcoming():
+    return render_template("upcoming.html", user = current_user)
+
+@views.route('/contact',  methods = ["GET","POST"])
+def contact():
+    return render_template("contact.html", user = current_user)
+
+@views.route('/acft',  methods = ["GET","POST"])
+def acft():
+    return render_template("acft.html", user = current_user)
