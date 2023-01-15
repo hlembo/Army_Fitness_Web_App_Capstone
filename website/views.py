ec69ca0e7a8bd9d099498af8a8225f58d7c593b7
Henry Lembo <lembohenry@gmail.com>
2023-01-14 20:39:32 -0500

    Progress!
    Implemented Dashboard, Records, and Output Visualization with Chart.js

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, url_for
+from flask import Blueprint, render_template, request, flash, url_for,redirect
 from flask_login import login_required, current_user
 from .models import Acft
 from . import db
@@ -9,6 +9,7 @@ views = Blueprint('views', __name__)
 #@login_required
 def home():
     return render_template("home.html", user = current_user)
+
 
 @views.route('/cadre', methods = ["GET","POST"])
 def cadre():
@@ -39,6 +40,7 @@ def contact():
     return render_template("contact.html", user = current_user)
 
 @views.route('/acft', methods = ["GET","POST"])
+@login_required
 def acft():
     if request.method == 'POST':
         plk = request.form.get('plank')
@@ -47,10 +49,11 @@ def acft():
         twomile = request.form.get('two-mile-run')
         mdl = request.form.get('deadlift')
         spt = request.form.get('standing-power-throw')
-        new_acft = Acft( twomilerun = twomile, mdl = mdl, spt = spt, hrp = hrp, sdc = sdc, plk=plk, user_id = current_user.id)
+        score1 = request.form.get("total-score")
+        new_acft = Acft( score = score1, twomilerun = twomile, mdl = mdl, spt = spt, hrp = hrp, sdc = sdc, plk=plk, user_id = current_user.id)
         db.session.add(new_acft)
         db.session.commit()
-        return render_template("profile.html", user=current_user)
+        return render_template("acft.html", user=current_user)
     return render_template("acft.html", user = current_user)
 
 
@@ -58,4 +61,29 @@ def acft():
 @login_required
 def profile():
         form_data = Acft.query.filter_by(user_id=current_user.id).all()
-        return render_template('profile.html', user=current_user, form_data=form_data)
+        labels = [acftscore.id for acftscore in form_data]
+        scores = [acftscore.score for acftscore in form_data]
+        new_data = {"labels": labels, "data": scores}
+        return render_template('profile.html', user=current_user, form_data=form_data, formatTime=formatTime, new_data=new_data)
+@views.route('/dashboard', methods = ["GET","POST"])
+@login_required
+def dashboard():
+    form_data = Acft.query.filter_by(user_id=current_user.id).all()
+    labels = [acftscore.id for acftscore in form_data]
+    scores = [acftscore.score for acftscore in form_data]
+    new_data = {"labels": labels, "data": scores}
+    return render_template("dashboard.html", user = current_user,  new_data=new_data, formatTime=formatTime)
+
+@views.route('/delete/<int:id>')
+def delete(id):
+    entry = Acft.query.get_or_404(id)
+    db.session.delete(entry)
+    db.session.commit()
+    flash("Entry deleted", category='success')
+    return redirect(url_for('views.profile'))
+
+def formatTime(seconds):
+    minutes = int(seconds) // 60
+    seconds = int(seconds) % 60
+    return f"{minutes}:{seconds:02d}"
+
